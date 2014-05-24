46dbd8683c004e2dd606b227af696d96956af4cc
Rodrigo Delduca <rodrigodelduca@gmail.com>
2014-05-24 15:57:53 -0300

    blah

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,10 +1,19 @@
 
+
+
+
 #include <Arduino.h>
 
+int ledPin = 13;
+
 void setup() {
-  
+    pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+    delay(1000);
 }
+
